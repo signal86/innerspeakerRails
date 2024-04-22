c154823aed704e2f57d6b5fceebc46cbd04a13fe
signal86 <diafakemail@gmail.com>
2024-04-22 13:57:27 -0500

    delete account in admin panel

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,18 +2,30 @@ class AccountController < ApplicationController
   def index
     if cookies.encrypted[:login].blank?
       redirect_to "/login"
+    elsif cookies.encrypted[:login] == "admin"
+      flash.now[:banner] = ServerHead.find(1).banner
+      render "index", status: 500
     end
   end
 
   def editSite
-    flash.now[:banner] = params[:site_editor][:banner]
+    flash.now[:banner] = ServerHead.find(1).banner
     @upd = ServerHead.find(1)
     @upd.banner = params[:site_editor][:banner]
+    returnMsg = ""
     if @upd.save
-      flash.now[:return] = "updated banner"
+      returnMsg = "updated banner, "
     else
-      flash.now[:return] = "failed to update banner"
+      returnMsg += "failed to update banner, "
     end
+    if params[:site_editor][:delete_account].length > 0
+      if Account.find_by(username: params[:site_editor][:delete_account]) != nil
+        @user = Account.find_by(username: params[:site_editor][:delete_account].to_s)
+        @user.destroy
+        returnMsg += "deleted user " + params[:site_editor][:delete_account]
+      end
+    end
+    flash.now[:return] = returnMsg
     render "index", status: 500
   end
 
